e8325e61d9ddf147937686ccb9bffac100522061
Carlo <carlomjavier@gmail.com>
2019-06-12 20:16:38 +0000

    generated tweets from database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,53 +7,100 @@ $(document).ready(function() {
     // --- our code goes here ---
     console.log("ready!");
 
-function createTweetElement(tweets) {
-    const user = $("<article>").addClass("tweet-content");
-    const header = $("<header>")
-        .addClass("tweet-header")
-        .appendTo(user);
-    $('<img>')
-        .addClass("logo")
-        .attr("src" , tweets.user.avatars.small)
-        .appendTo(header);
-    $('<p>')
-        .text(tweets.user.handle)
-        .appendTo(header);
-    $('<h2>')
-        .text(tweets.user.name)
-        .appendTo(header);
-    $('<p>')
-        .text(tweets.content.text)
-        .appendTo(user);
-    $('<footer>')
-        .addClass("tweet-bottom")
-        .text(tweets.created_at)
-        .appendTo(user);
-        
-return user;
-}
+  function renderTweets(tweets) {
+      // loops through tweets
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+      tweets.forEach((tweet, i) => {
+        var $tweet = createTweetElement(tweet);
+        $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+      });
+      // Test / driver code (temporary)
+    // console.log($tweet); // to see what it looks like
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-    "user": {
+  }
+
+  function createTweetElement({ user, content, created_at }) {
+    console.log('inside createtweet', user);
+      const tweet = $("<article>").addClass("tweet-content");
+      const header = $("<header>")
+          .addClass("tweet-header")
+          .appendTo(tweet);
+      $('<img>')
+          .addClass("logo")
+          .attr("src" , user.avatars.small)
+          .appendTo(header);
+      $('<p>')
+          .text(user.handle)
+          .appendTo(header);
+      $('<h2>')
+          .text(user.name)
+          .appendTo(header);
+      $('<p>')
+          .text(content.text)
+          .appendTo(tweet);
+      $('<footer>')
+          .addClass("tweet-bottom")
+          .text(created_at)
+          .appendTo(tweet);
+      //     //
+    return tweet;
+  }
+
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const data = [
+    {
+      "user": {
         "name": "Newton",
         "avatars": {
-            "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-            "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-            "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
         },
         "handle": "@SirIsaac"
-    },
-    "content": {
+      },
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "created_at": 1461116232227
-}
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
-var $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
+  
+
 
 });
+
+// });
