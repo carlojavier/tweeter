34466da19af47afb1d6c2926861b708bfdb8d4ce
Carlo <carlomjavier@gmail.com>
2019-06-12 23:38:32 +0000

    fixed fetching problems throgh ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,104 +3,110 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
-    // --- our code goes here ---
-    console.log("ready!");
+$(document).ready(function () {
+  // --- our code goes here ---
+  console.log("ready!");
 
   function renderTweets(tweets) {
-      tweets.forEach((tweet, i) => {
-        var $tweet = createTweetElement(tweet);
-        $('#tweet-container').append($tweet); 
-      });
-     
+    tweets.forEach((tweet, i) => {
+      const $tweet = createTweetElement(tweet);
+      $('#tweet-container').append($tweet);
+    });
   }
 
   function createTweetElement({ user, content, created_at }) {
-    console.log('inside createtweet', user);
-      const tweet = $("<article>").addClass("tweet-content");
-      const header = $("<header>")
-          .addClass("tweet-header")
-          .appendTo(tweet);
-      $('<img>')
-          .addClass("logo")
-          .attr("src" , user.avatars.small)
-          .appendTo(header);
-      $('<p>')
-          .text(user.handle)
-          .appendTo(header);
-      $('<h2>')
-          .text(user.name)
-          .appendTo(header);
-      $('<p>')
-          .text(content.text)
-          .appendTo(tweet);
-      $('<footer>')
-          .addClass("tweet-bottom")
-          .text(created_at)
-          .appendTo(tweet);
-      //     //
+    // console.log('inside createtweet', user);
+    const tweet = $("<article>").addClass("tweet-content");
+    const header = $("<header>")
+      .addClass("tweet-header")
+      .appendTo(tweet);
+    $('<img>')
+      .addClass("logo")
+      .attr("src", user.avatars.small)
+      .appendTo(header);
+    $('<p>')
+      .text(user.handle)
+      .appendTo(header);
+    $('<h2>')
+      .text(user.name)
+      .appendTo(header);
+    $('<p>')
+      .text(content.text)
+      .appendTo(tweet);
+    $('<footer>')
+      .addClass("tweet-bottom")
+      .text(created_at)
+      .appendTo(tweet);
+    //     //
     return tweet;
   }
 
-  $(() => {
-    const BASE_URL = '/tweets';
+  // const BASE_URL = '/tweets';
+
+  function loadTweets() {
+      $.get("/tweets")
+      .then((data) => {
+        renderTweets(data)
+      })
+  }
+  loadTweets();
 
   $('#sentTweet').on('submit', (event) => {
     event.preventDefault();
-    $.post(`${BASE_URL}`, $('#sentTweet').serialize(),(newTweet) => {
-      // createTweet(newTweet);
-      console.log("something")
+    $.post(`/tweets`, $('#sentTweet').serialize(), (newTweet) => {
+      console.log(newTweet)
     })
-
   })
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": {
+  //         "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+  //         "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  //       },
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": {
+  //         "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+  //         "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+  //       },
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Johann von Goethe",
+  //       "avatars": {
+  //         "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+  //         "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+  //       },
+  //       "handle": "@johann49"
+  //     },
+  //     "content": {
+  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+  //     },
+  //     "created_at": 1461113796368
+  //   }
+  // ];
 
-  renderTweets(data);
-}) 
-});
+  // renderTweets(data);
+
+
+})
